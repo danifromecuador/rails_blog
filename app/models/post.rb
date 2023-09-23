fa6277d8c171fe42b16d3c690ea9f0ec4c48b917
dani <danifromecuador@gmail.com>
2023-09-22 19:36:44 -0500

    Create relationships for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
 end
