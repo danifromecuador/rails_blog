207b95a61231c3507a986ceca158cffd7346eee2
dani <danifromecuador@gmail.com>
2023-09-22 22:47:25 -0500

    Add methods for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  after_create :update_user_post_counter
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_user_post_counter
+    user.update_column(post_counter: user.posts.count)
+  end
 end
