d529cf12868f75f7d0171d11bef312f8a90d5423
dani <danifromecuador@gmail.com>
2023-09-27 13:28:39 -0500

    Create route, controller, and view for all posts from a given user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,8 @@ class UsersController < ApplicationController
   def show
     # @user = User.find(params[:id])
   end
+
+  def posts
+    # @posts = Post.where(user_id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "users#posts", as: :user_posts
 end
