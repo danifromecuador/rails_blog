cd781747d6d61df86b82e1e16bb9d293434c88ce
dani <danifromecuador@gmail.com>
2023-09-27 12:37:28 -0500

    Put the methods at the end of the class User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
-  has_many :comments
-  has_many :likes
-  attribute :post_counter, :integer, default: 0
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :liker_id
+  # attribute :post_counter, :integer, default: 0
+
+  validates :name, presence: true
+  validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
-
-  validates :name, presence: true
-  validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
 end
