20231d83a11aeed1b77cfbde3f007c64f88ac009
dani <danifromecuador@gmail.com>
2023-09-28 14:41:51 -0500

    Add root route and Use resources instead GET

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-  get "/users/:id/posts", to: "users#posts", as: :user_posts
-  get "/users/:id/posts/:id", to: "users#post", as: :user_post
+  root "users#index"
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
