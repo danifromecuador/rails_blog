59c20c5ffb03d90a709336d04fa66588e764f4f7
dani <danifromecuador@gmail.com>
2023-09-28 22:26:53 -0500

    Create 2 more specs for requests/posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,9 +5,25 @@ RSpec.describe '/users/:user_id/posts', type: :request do
 
   describe 'GET /index' do
     it 'renders a successful response' do
+      get "/users/#{first_user.id}/posts"
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /index' do
+    it 'renders an h2 text' do
+      get "/users/#{first_user.id}/posts"
+
+      expect(response.body).to include('<h2>List of all posts for a given user</h2>')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders a successful response' do
       first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
 
-      get user_posts_url(first_user)
+      get "/users/#{first_user.id}/posts/#{first_post.id}"
 
       expect(response).to be_successful
     end
