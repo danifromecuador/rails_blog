3b6f548a75435850280390fcf6f0c68627301de6
dani <danifromecuador@gmail.com>
2023-09-28 21:57:23 -0500

    Create 2 more specs for requests/users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,11 +9,28 @@ RSpec.describe '/users', type: :request do
     }
   end
 
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      User.create! valid_attributes
+      get users_url
+      expect(response).to be_successful
+    end
+  end
+
   describe 'GET /show' do
     it 'renders a successful response' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response).to be_successful
+      expect(response.body).to include('<h2>Details of a given user</h2>')
     end
   end
 end
