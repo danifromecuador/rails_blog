737c1d1793afc6eeb8f3821098c4649bc0178e88
dani <danifromecuador@gmail.com>
2023-09-28 23:32:47 -0500

    Create 3 tests for index of posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,28 +4,19 @@ RSpec.describe '/users/:user_id/posts', type: :request do
   let(:first_user) { User.create(name: 'John', photo: 'https://example.com/john.jpg', bio: 'Some bio') }
 
   describe 'GET /index' do
-    it 'renders a successful response' do
+    it 'response status is successful' do
       get "/users/#{first_user.id}/posts"
-
       expect(response).to be_successful
     end
-  end
 
-  describe 'GET /index' do
-    it 'renders an h2 text' do
+    it 'renders the correct template' do
       get "/users/#{first_user.id}/posts"
+      expect(response).to render_template('posts/index')
+    end
 
+    it 'renders the right placeholder text' do
+      get "/users/#{first_user.id}/posts"
       expect(response.body).to include('<h2>List of all posts for a given user</h2>')
-    end
-  end
-
-  describe 'GET /show' do
-    it 'renders a successful response' do
-      first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-
-      get "/users/#{first_user.id}/posts/#{first_post.id}"
-
-      expect(response).to be_successful
     end
   end
 end
