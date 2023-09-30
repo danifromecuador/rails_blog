b759a51dfddc9ade8eaf55ee5315c00e0c79459d
dani <danifromecuador@gmail.com>
2023-09-30 01:39:26 -0500

    Create header of posts index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   def index
-    # @posts = Post.where(user_id: params[:id])
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
   end
 
-  def show
-    # @post = Post
-    #   .where(user_id: params[:id])
-    #   .where(id: params[:id])
-    #   .first
-  end
+  # def show
+  #   @post = Post
+  #     .where(user_id: params[:id])
+  #     .where(id: params[:id])
+  #     .first
+  # end
 end
