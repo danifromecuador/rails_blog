b7bc536b35873648a651919c9728b319bb38df36
dani <danifromecuador@gmail.com>
2023-09-30 16:17:20 -0500

    Create routes, controller, button and view for add a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,26 @@ class PostsController < ApplicationController
   def author_name
     User.find_by(id: author_id)&.name
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.create(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
