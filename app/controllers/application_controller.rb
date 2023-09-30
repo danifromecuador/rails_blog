cc6050fdfaad3b7f501a72fcae0f08ca7889ded9
dani <danifromecuador@gmail.com>
2023-09-30 12:47:03 -0500

    Make current user data available to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.first
+  end
+  helper_method :current_user
 end
