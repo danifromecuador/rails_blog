5a0ce93d54a8e3f4bc4f88942dcc14ce2a163ef1
dani <danifromecuador@gmail.com>
2023-09-30 03:17:33 -0500

    Limit the showed posts for a user show to 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order(created_at: :desc)
+    @two_more_recent_posts = @user.posts.order(created_at: :desc).limit(2)
   end
 end
