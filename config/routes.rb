18fb5eeaed2ab73e21beb3a0d2aa10109ef52d30
dani <danifromecuador@gmail.com>
2023-09-30 13:04:22 -0500

    Add 2 new sub routes new and create for posts route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   root "users#index"
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
   end
 end
