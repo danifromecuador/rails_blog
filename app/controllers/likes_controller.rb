88e4225b3a9e789aa1cb4c2d755e77406475f1b8
dani <danifromecuador@gmail.com>
2023-09-30 19:17:37 -0500

    Fix eslint and rubocop errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,9 +2,9 @@ class LikesController < ApplicationController
   def create
     @user = current_user
     @post = Post.find(params[:post_id])
-    
+
     @like = Like.new(user: @user, post: @post)
-    
+
     if @like.save
       @post.increment!(:likes_counter)
       redirect_to user_post_path(@user, @post), notice: 'Liked successfully.'
