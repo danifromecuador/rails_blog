073e76cee6666d9daa00447c43242036b17a74d9
Alexandre <alexsales001@hotmail.com>
2023-10-04 14:05:50 -0300

    implemented the tests

--- spec/user_show_spec.rb
+++ spec/user_show_spec.rb
@@ -37,3 +37,50 @@ RSpec.describe 'UsersShow', type: :system do
       text: 'This is the content of the third post by Tom.'
     )
   end
+
+  it 'I can see the users profile picture' do
+    tom = User.find_by(name: 'Tom')
+    visit user_path(tom)
+    expect(page).to have_css("img[src*='https://picsum.photos/seed/1/200']")
+  end
+
+  it 'I can see the users name' do
+    tom = User.find_by(name: 'Tom')
+    visit user_path(tom)
+    expect(page).to have_content('Tom')
+  end
+
+  it 'I can see the number of posts the user has written.' do
+    tom = User.find_by(name: 'Tom')
+    visit user_path(tom)
+    expect(page).to have_content('Number of posts: 3')
+  end
+
+  it 'I can see the users bio' do
+    tom = User.find_by(name: 'Tom')
+    visit user_path(tom)
+    expect(page).to have_content('An user with a bio.')
+  end
+
+  it 'I can see the users first 3 posts' do
+    tom = User.find_by(name: 'Tom')
+    visit user_path(tom)
+    expect(page).to have_content('First Post by Tom')
+    expect(page).to have_content('Second post by Tom')
+    expect(page).to have_content('Third post by Tom')
+  end
+
+  it 'When I click a users post, it redirects me to that posts show page' do
+    tom = User.find_by(name: 'Tom')
+    visit user_path(tom)
+    click_link('First Post by Tom')
+    expect(page).to have_current_path(user_post_path(tom, Post.find_by(title: 'First Post by Tom')))
+  end
+
+  it 'When I click the see all posts button, it redirects me to the users posts index page' do
+    tom = User.find_by(name: 'Tom')
+    visit user_path(tom)
+    click_link('See all posts')
+    expect(page).to have_current_path(user_posts_path(tom))
+  end
+end
