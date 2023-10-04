ffc5b424529de3c16cfc3bb140fa6c4d7df8e430
Alexandre <alexsales001@hotmail.com>
2023-10-04 13:54:32 -0300

    add tests logic

--- spec/post_index_spec.rb
+++ spec/post_index_spec.rb
@@ -37,3 +37,59 @@ RSpec.describe 'UsersIndex', type: :system do
       text: 'This is the first comment on the first post by Tom.'
     )
   end
+
+  it 'I can see the user profile picture' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    expect(page).to have_current_path(user_posts_path(tom))
+  end
+
+  it 'I can see the user name' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    expect(page).to have_content('Tom')
+  end
+
+  it 'I can see the number of posts the user has written.' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    expect(page).to have_content('Number of posts: 3')
+  end
+
+  it 'I can see a post title' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    expect(page).to have_content('First Post by Tom')
+  end
+
+  it 'I can see some of the post body' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    expect(page).to have_content('This is the content of the first post by Tom.')
+  end
+
+  it 'I can see the first comments on a post' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    expect(page).to have_content('This is the first comment on the first post by Tom.')
+  end
+
+  it 'I can see how many comments a post has' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    expect(page).to have_content('Comments: 1')
+  end
+
+  it 'I can see how many likes a post has' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    expect(page).to have_content('Likes: 0')
+  end
+
+  it 'When I click on a post, it redirects me to that post show page' do
+    tom = User.find_by(name: 'Tom')
+    visit user_posts_path(tom)
+    click_link('First Post by Tom')
+    expect(page).to have_current_path(user_post_path(tom, Post.find_by(title: 'First Post by Tom')))
+  end
+end
