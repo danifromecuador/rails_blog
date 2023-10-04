7ca3462f9c54940702d1909b892307535cc9f26c
Alexandre <alexsales001@hotmail.com>
2023-10-04 13:53:42 -0300

    implement first steps for test

--- spec/post_index_spec.rb
+++ spec/post_index_spec.rb
@@ -1,1 +1,39 @@
 require 'rails_helper'
+
+RSpec.describe 'UsersIndex', type: :system do
+  before do
+    FactoryBot.create(
+      :user,
+      name: 'Tom',
+      photo: 'https://picsum.photos/seed/1/200',
+      bio: 'An user with a bio.'
+    )
+
+    FactoryBot.create(
+      :post,
+      author: User.find_by(name: 'Tom'),
+      title: 'First Post by Tom',
+      text: 'This is the content of the first post by Tom.'
+    )
+
+    FactoryBot.create(
+      :post,
+      author: User.find_by(name: 'Tom'),
+      title: 'Second post by Tom',
+      text: 'This is the content of the second post by Tom.'
+    )
+
+    FactoryBot.create(
+      :post,
+      author: User.find_by(name: 'Tom'),
+      title: 'Third post by Tom',
+      text: 'This is the content of the third post by Tom.'
+    )
+
+    FactoryBot.create(
+      :comment,
+      post: Post.find_by(title: 'First Post by Tom'),
+      user: User.find_by(name: 'Tom'),
+      text: 'This is the first comment on the first post by Tom.'
+    )
+  end
