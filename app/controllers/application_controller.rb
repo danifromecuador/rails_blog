cfc981fc933a1ce0ed893de1df2a8888cecbbcb2
dani <danifromecuador@gmail.com>
2023-10-05 08:00:34 -0500

    Remove current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def current_user
-    @current_user ||= User.find(params[:user_id]) if params[:user_id]
-  end
-  helper_method :current_user
 end
