cdd25624eb8bf5b26e51de81f72a69eb0e17709e
dani <danifromecuador@gmail.com>
2023-10-06 12:34:44 -0500

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :liker_id
-  attribute :post_counter, :integer, default: 0 
+  attribute :post_counter, :integer, default: 0
 
   validates :name, presence: true
   validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
