f0e33de551a08a2faba54abf9df5a89113eb607d
Alexandre <alexsales001@hotmail.com>
2023-10-06 14:08:48 -0300

    Add confirmable property to devise in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :liker_id
-  attribute :post_counter, :integer, default: 0
+  attribute :post_counter, :integer, default: 0 
 
   validates :name, presence: true
   validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
